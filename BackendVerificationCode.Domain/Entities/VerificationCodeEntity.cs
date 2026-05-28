a9cef77e50b4d9c546098dbfdf0d6f629f4bfa94
Ivonapp <ivona.v.hellsten@hotmail.com>
2026-05-28 15:16:21 +0200

    I have written the entities and ensured that the database is connected to Azure. I have also reviewed the different schemas to understand how that part works, verified that all schemas are visible, and structured my various microservices.
    Within my entities, I have created an ExpiresTime. Depending on the lifespan of the verification code, this will connect to a JavaScript function in the frontend that handles the countdown timer for the user, showing how much time they have left to enter the code received in their email.
    
    I have created a new migration and updated the database. Next, I will start working on the service and controller to integrate this part with Gabriel's API. I chose a REST API for this because the user navigates through the website and lands on my page, so it needs to be fast. I decided to not use a ServiceBus since it is better suited for tasks where an immediate, real-time response is not required.

--- BackendVerificationCode.Domain/Entities/VerificationCodeEntity.cs
+++ BackendVerificationCode.Domain/Entities/VerificationCodeEntity.cs
@@ -4,9 +4,17 @@ using System.Text;
 
 namespace BackendVerificationCode.Domain.Entities;
 
-public class VerificationCodeEntity
+public class VerificationCode
 {
+    public int Id { get; set; }                         //ID för den specifika raden i databasen
+    public string Code { get; set; } = string.Empty;    // Den 7-siffriga koden
+    public string Email { get; set; } = string.Empty;   // Kopplar koden till rätt användare 
+    public bool IsUsed { get; set; } = false;           // en säkerhetsspärr som bara ser till att koden används EN gång. Scenario: om en person ska skriva in koden och en obehörig sen ser koden, så KAN båda sktiva in koden samtidigt UTAN denna.
 
-    //Test
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Exakt tid näe användaren tryclte in på sidan.
+    public DateTime ExpiresAt { get; set; }
+    // ExpiresAt: Om personen klickar in på min sida klockan 12:00 (CreatedAt) så kommer ExpiresAt att skriva ut 12.02 om koden är aktiv i 2 minuter. (eller om countdown är på 1 min, så kommer det stå 12.01)
+    // Ska sen skapa en javascript countdown timer i frontend som tajmar in sig med ExpiresAt.
+    // KOD: ExpiresAt = DateTime.UtcNow.AddMinutes(2); // I koden sen kan jag t.ex. använda denna där jag skriver in exakt antal minuter för nedräkningen. 
 
 }
--- BackendVerificationCode.Infrastructure/Migrations/20260528120718_AddVerificationCodeTable.Designer.cs
+++ BackendVerificationCode.Infrastructure/Migrations/20260528120718_AddVerificationCodeTable.Designer.cs
@@ -4,6 +4,7 @@ using BackendVerificationCode.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BackendVerificationCode.Infrastructure.Migrations
 {
     [DbContext(typeof(VerificationDbContext))]
-    partial class VerificationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260528120718_AddVerificationCodeTable")]
+    partial class AddVerificationCodeTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
